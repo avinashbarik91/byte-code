5561eba5172c5c97a4aa3a76cc47ce135860ce67
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-02-08 23:31:02 +1100

    Test 4

--- public/index.php
+++ public/index.php
@@ -3,10 +3,9 @@
 <head>
 	<title>Avinash Barik | Software Engineer</title>
 </head>
-<body style="background-color: #f9c5ce; ">
+<body style="background-color: #3fb9f3; ">
 	<div style="width:100%; text-align: center; color: white; font-family: Verdana;">
-		<h1>This is a test!</h1>
-		<h3>I Love You!</h3>
+		<h1>This site is under construction</h1>		
 	</div>
 </body>
 </html>
