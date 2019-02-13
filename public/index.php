4f7e339b9ea781445b8336274b4140480e90813c
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-02-13 22:08:15 +1100

    Setup basic html page

--- public/index.php
+++ public/index.php
@@ -1,11 +1,106 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta http-equip="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom_styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 	<title>Avinash Barik | Software Engineer</title>
 </head>
-<body style="background-color: #3fb9f3; ">
-	<div style="width:100%; text-align: center; color: white; font-family: Verdana;">
-		<h1>This site is under construction</h1>		
+<body>
+	<nav class="navbar fixed-top navbar-dark bg-dark navbar-expand-sm mb-5">
+		<div class="container">
+			<a href="#" class="navbar-brand">
+				Avinash Barik				
+			</a>
+			<div class="navbar-nav">
+				<a class="nav-item nav-link" href="#experience">About Me</a>
+				<a class="nav-item nav-link" href="#">Portfolio</a>
+				<a class="nav-item nav-link" href="#">Blog</a>
+				<a class="nav-item nav-link" href="#">Contact Me</a>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container pt-5">
+		<div class="content">
+			
+			<div class="row mb-5 bg-light p-5"">
+				<section class="col-md-12 text-center mb-3">
+					<img class="rounded-circle" src="images/avinash_barik_prof_pic.jpg" style="width:20%;">
+				</section>
+				<section class="col-md-12 text-center">
+					<h2>Avinash Barik</h2>
+					<h4>Software Engineer at Pixel Together | AWS Certified Solutions Architect - Associate</h4>
+					<h5 class="text-secondary">Melbourne, Australia</h5>
+				</section>
+			</div>
+			
+			<div class="row mb-3">
+				<section class="col-md-2">
+					<h3 class="text-secondary text-md-left text-center">Summary</h3>
+				</section>
+				<section class="col-md-10 text-md-left text-center">
+					<p>
+						Technically proficient software engineering professional with hands-on experience in developing software features, architecture and tools for traditional and cloud-based industries.
+					</p>
+
+					<p>	
+						I am currently working as software engineer at Pixel Together, developing new features and tools for the Pixel Together platform as well as for World Academy of Sport. The pleasure derived from problem-solving and working on a product that truly impacts people's lives, is what drives me. I have a broad experience in the software industry which includes software development, automation testing and user-experience design. 
+					</p>
+
+					<p>	
+						I believe my experience of working in smaller, highly productive teams as well as bigger teams, which were very diverse, in terms of size, location and culture, has enabled me to develop strong work ethics and has developed my sense of cultural awareness further. I believe I have the ability to think critically and take responsibility, which has enabled me to work well under pressure while still delivering high quality work.
+					</p>
+
+					<p>	
+						My interests include following and learning the latest trends in the field of technology, playing/following cricket and badminton and graphic designing.
+					</p>
+				</section>
+			</div> <!-- End row -->
+
+			<div id="experience" class="row mb-3">
+				<section class="col-md-12">
+					<h3 class="text-secondary text-md-left text-center">Experience</h3>
+				</section>
+				<section class="col-md-2">
+					<h4 class="text-md-left text-center">Pixel Together</h4>
+				</section>
+				<!-- <section class="col-md-1">
+					<div style="background: url('images/bg-1.png') repeat-y; width:50px;"></div>
+				</section> -->
+				<section class="col-md-10 text-md-left text-center">
+					<h5 clas="col-md-12">Software Engineer</h5>
+					<p class="text-secondary">November 2017-Present <br/> Melbourne, Australia</p>
+					
+					<p>Pixel Together is a flexible, fully customizable and powerful website building platform for designers & creatives and one of the fastest growing startups of Australia. As part of the job, I also build software for The World Academy of Sport which delivers wide variety of learning and training programmes for Olympics, Paralympics & other major international sporting federations across the world.</p>
+
+					<p><u>Primary responsibilities include:</u></p> 
+
+					<li>Developing software features for Pixel Together and the World Academy of Sport.</li>
+					<li>Developing supporting databases and front-end software.</li> 
+					<li>Suggest and implement solutions and best practices for required architechture on AWS.</li>
+					<li class="mb-3">Software testing of the developed system and defect resolution.</li>
+
+					<p><u>Some key projects completed:</u></p>
+
+					<li>Site Analytics Tool – Analyze, comprehend and visualize the site traffic, in form of interactive charts, based on filters such as Unique Visitors, Popular pages and more.</li>
+					<li>RSS Feed Generator - Auto-generation tool for websites created on Pixel Together platform.</li>
+					<li>Sitemap Generator - Analyses all pages within custom websites and generates a sitemap based on page link popularity and link depth.</li>
+					<li>User Funnel – Rewrite user funnel to cut compute time and help visualize user behavior on the Pixel Together platform.</li> 
+				</section>
+			</div> <!-- End row -->
+		</div><!-- End content -->	
+	</div><!-- End container -->
+	<div class="bg-dark" style="height: 50px;">
+		
 	</div>
+
+	<script src="js/jquery.slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
