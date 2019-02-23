abcee14e87e086bd3cab0bebd46a5fe50923894f
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-02-23 17:16:14 +1100

    Move header and footer to different files

--- public/index.php
+++ public/index.php
@@ -11,30 +11,9 @@
 	<title>Avinash Barik | Software Engineer</title>
 </head>
 <body>
-	<!-- <nav class="navbar text-light navbar-expand-sm mb-5 fixed-top">
-		<div class="container">
-			<span href="#" class="navbar-brand">
-				Avinash Barik				
-			</span>
-
-			<button class="navbar-toggler" type="button"
-				data-toggle="collapse" data-target="#myTogglerNav" aria-controls="myTogglerNav" 
-				aria-expanded="false" 
-				aria-label="Toggle Navigation">
-					<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="myTogglerNav">
-				<div class="navbar-nav ml-auto">
-					<a class="nav-item nav-link" href="#experience">About Me</a>
-					<a class="nav-item nav-link" href="#">Portfolio</a>
-					<a class="nav-item nav-link" href="#">Blog</a>
-					<a class="nav-item nav-link" href="#">Contact Me</a>
-				</div>
-			</div>
-		</div>
-	</nav> -->
-
+		
+		<!-- Add header -->
+		<?php include_once('header.inc'); ?>
 			
 		<div class="intro-bio-section text-center">
 			<div class="intro-bio-text-area">
@@ -70,7 +49,7 @@
 						</p>
 						<p>I started this website to blog about tips, tricks and my day to day learnings, related to software and technology in general. I aim to write a few articles each week related to specific software problems I came across and how I went about solving it.</p>
 
-						<a href="#blog-section" <button type="button" class="btn btn-dark-custom">Visit my Blog</button></a>						
+						<a href="#blog-section" class="btn btn-dark-custom">Visit my Blog</a>						
 					</section>
 				</div> <!-- End row -->
 			</div><!-- End container -->
@@ -91,14 +70,10 @@
 
 						<p>
 							Problem-solving and working on products that impacts people's lives, is what drives me. My experience in the software industry includes software development, automation testing and user-experience design. 
-						</p>
-
-						<p>
-							I am currently working as software engineer at Pixel Together, developing new features and tools for the Pixel Together platform as well as for World Academy of Sport. 
-						</p>
+						</p>						
 
 						<p>	
-							When I am not working I love to indulge in sports, especially cricket and badminton.
+							When I am not working, I love to indulge in sports, especially cricket and badminton.
 						</p>
 					</section>
 				</div> <!-- End row -->
@@ -107,7 +82,7 @@
 
 		<div class="container-wrapper mb-5 pb-5">
 			<div id="experience" class="container pt-2">
-				<div class="row mb-4">
+				<div class="row mb-4 px-4 px-md-2">
 					<section class="col-md-12 pt-5">
 						<h3 class="section-title text-center pb-4">WORK EXPERIENCE</h3>
 					</section>
@@ -122,8 +97,8 @@
 					</section>
 					<section class="col-md-9 text-left">					
 						<h5 class="job-title">Software Engineer</h5>
-						<span class="expand-icon"><img src="images/down1.png" width="30"></span>
-						<span class="contract-icon"><img src="images/up1.png" width="30"></span>
+						<span class="expand-icon"><img src="images/down1.png" width="20"></span>
+						<span class="contract-icon"><img src="images/up1.png" width="20"></span>
 						<section class="job-description">
 							<p class="text-secondary">November 2017-Present <br/> Melbourne, Australia</p>
 							
@@ -146,7 +121,7 @@
 					</section>
 				</div> <!-- End row -->
 
-				<div class="row mb-5">					
+				<div class="row mb-5 px-4 px-md-2">					
 					<section class="col-md-2">						
 					</section>
 					<section class="d-none d-md-block col-md-1 mt-1">
@@ -155,8 +130,8 @@
 					</section>
 					<section class="col-md-9 text-left">					
 						<h5 class="job-title">Software Engineering Intern</h5>
-						<span class="expand-icon"><img src="images/down1.png" width="30"></span>
-						<span class="contract-icon"><img src="images/up1.png" width="30"></span>
+						<span class="expand-icon"><img src="images/down1.png" width="20"></span>
+						<span class="contract-icon"><img src="images/up1.png" width="20"></span>
 						<section class="job-description">
 							<p class="text-secondary">Aug 2017 – Oct 2017 <br/> Melbourne, Australia</p>
 							
@@ -165,9 +140,42 @@
 							<p>The Analytics dshboard consisted of metrics such Page Views, Unique Visitors, Popular pages, Users by Device etc.</p>
 						</section>
 					</section>
+				</div> <!-- End row -->			
+
+				<div class="row mb-4 pt-4 px-4 px-md-2">					
+					<section class="col-md-2">						
+						<figure>
+							<img src="images/tcs.png" alt="TCS" width="170">
+						</figure>
+					</section>
+
+					<section class="d-none d-md-block col-md-1 mt-1">
+						<div class="timeline-start"></div>
+						<div class="timeline"></div>
+					</section>
+
+					<section class="col-md-9 text-left">					
+						<h5 class="job-title">Systems Engineer (Software Test Automation)</h5>
+						<span class="expand-icon"><img src="images/down1.png" width="20"></span>
+						<span class="contract-icon"><img src="images/up1.png" width="20"></span>
+						<section class="job-description">
+							<p class="text-secondary">Dec 2013 – Jan 2016 <br/> Bengaluru, India</p>
+							
+							<p>Developed automated functional and regression test scripts. Created software test plans along with clients to help transition from a waterfall to agile testing model.</p> 
+							<p>
+								<li>Developed a reporting tool that aggregates and generates reports from 1800 servers using Java.</li>
+ 								<li>Led a team of two engineers and implemented a hybrid framework for software automation testing using Selenium WebDriver and Java.</li>
+								<li>Developed and delivered test automation scripts for a tool on Motorolla PDA, for large US based retail chain.</li>
+							</p>
+						</section>
+					</section>
 				</div> <!-- End row -->
 
-				<div class="row mb-4 pt-4">					
+
+				<div class="row mb-4 pt-4 px-4 px-md-2">	
+					<section class="col-md-12 pt-5 pb-4">
+						<h3 class="section-title text-center pb-4">ADDITIONAL EXPERIENCE</h3>
+					</section>				
 					<section class="col-md-2">						
 						<figure>
 							<img src="images/swin.png" alt="Swinburne" width="170">
@@ -181,8 +189,8 @@
 
 					<section class="col-md-9 text-left">					
 						<h5 class="job-title">Academic Sessional Tutor (Part Time)</h5>
-						<span class="expand-icon"><img src="images/down1.png" width="30"></span>
-						<span class="contract-icon"><img src="images/up1.png" width="30"></span>
+						<span class="expand-icon"><img src="images/down1.png" width="20"></span>
+						<span class="contract-icon"><img src="images/up1.png" width="20"></span>
 						<section class="job-description">
 							<p class="text-secondary">Mar 2017 – Oct 2017 <br/> Melbourne, Australia</p>
 							
@@ -204,7 +212,7 @@
 				<section id="blog-section" class="col-md-12">
 						<h3 class="section-title text-center pb-4">BLOG</h3>
 				</section>
-				<div class="row mb-5">					
+				<div class="row mb-5 px-4 px-md-2">					
 					<section class="offset-md-2 col-md-3">
 						<figure class="pt-1">
 							<img class="rounded"src="images/blog1.jpeg" alt="Blog 1" width="250" height="180">
@@ -243,14 +251,14 @@
 			</div><!-- End container wrapper-->
 		</div>
 
-	<div class="footer bg-dark" style="height: 50px;">
-		<p class="footer-text text-center mb-0 text-light">Developed By Avinash Barik</p>
-	</div>
-
+	<!-- Add Footer -->
+	<?php include_once('footer.inc'); ?>	
+	
 	<script src="js/jquery.slim.min.js"></script>
 	<script src="js/jquery.min.js"></script>
+	<script src="js/custom.js"></script>
 	<script src="js/popper.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<script src="js/custom.js"></script>
+	
 </body>
 </html>
