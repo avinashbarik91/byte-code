dfca318d7d9423fd6e127b32e45e17366b57bfec
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-03-16 19:53:42 +1100

    Add interests

--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@
 	<meta http-equiv="x-ua-compatible" content="ie-edge">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/custom_styles.css">
-	<link href="https://fonts.googleapis.com/css?family=Lato|Nunito|Oswald|Raleway|Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins|Nunito|Oswald|Raleway|Roboto" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<title>Avinash Barik | Software Engineer</title>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="100">
@@ -24,27 +25,65 @@
 
 	<div class="content">
 
-		<div id="about-me" class="container-wrapper px-5">
+		<div id="about-me" class="container-wrapper">
 			<div class="container p-5">	
 				<div class="row">
 					<section class="col-md-12">
-						<h3 class="section-title text-center pb-4">WHO AM I?</h3>
+						<h3 class="section-title text-center pb-4">ABOUT ME</h3>
 					</section>
-					<section class="section-detail offset-md-2 col-md-8 text-center">
-						<p>
+					<section class="section-detail col-md-12 text-justify mb-3">
+						<p class="text-center">
 							I am an engineering professional with hands-on experience in developing and designing
 							software features, architecture and tools for traditional and large-scale distributed cloud-based
 							industries.
 						</p>
 
-						<p>
-							Problem-solving and working on products that impacts people's lives, is what drives me. My experience in the software industry includes software development, automation testing and user-experience design. 
-						</p>						
+						<p>The world of computers has fascinated me ever since I saw one at the age of seven. As early as I can remember, I've always wanted to build things. I was that kid who would deliberately break apart his toys and re-assemble them to understand how it works. It was just so much fun! The world of the software helped me realize this passion of creating things in a meaningful way. In this world I could build things and see them come to life. </p> 
 
-						<p>	
-							When I am not working, I love to indulge in sports, especially cricket and badminton.
-						</p>
+						<p>In this world I was a creator! </p>
+
+						<p>The yearning to understand the working of computers and softwares has been one of my desires while growing up. Although my heart skipped a beat every time I crashed my system, more often than not, the curiosity to know what all an application could do and how they worked made me learn something new. This desire grew many folds during my school and by the time I reached high school, I already had tried my hands in basic graphic design, 2D game development and web development.</p> 
+
+						<p>I taught myself various design and development tools and started freelancing during college. It was a source of immense joy, pleasure and satisfaction. In university, I actively participated in extra-curricular activities, hosted and helped organize some of the biggest university level fests and spearheaded many design teams. The experiences have taught me to acknowledge a setback, evaluate it with rationality, to be goal-oriented and above all, to be diligent and meticulous.</p> 
+
+						<p>Fast forward five years and I now have broad experience in the software industry which includes software development, automation testing and user-experience design, all of this while still being fascinated by software. My experience of working with talented and diverse teams, in terms of size, location and culture, has enabled me to develop strong work ethics. I believe I have the ability to think critically and take responsibility, which has enabled me to work well under pressure while still delivering high quality work.</p>
+
+						<p>All of these experiences, have only increased my hunger to build things and have made me realize that it's here, in the world of software, where my passion lies. I continue to be amazed by what software can do, how it can impact people's lives and the limitless possibilities it presents.</p>	
+						
 					</section>
+					<section class="col-md-12 text-center">
+						<h3 class="section-title pb-4">I LOVE TO</h3>
+						<div class="row interests">
+							<section class="col-md-3">
+								<div>
+									<i class="fas fa-code"></i>
+									<h5>CODE</h5>
+									<p>PHP, JAVA, JS, HTML/CSS, Bootstrap, SQL, AWS <br/> Current focus: Machine Learning.</p>
+								</div>
+							</section>
+							<section class="col-md-3">
+								<div>
+									<i class="fas fa-object-group"></i>
+									<h5>DESIGN</h5>
+									<p>I have designed logos, brochures etc for companies.</p>
+								</div>
+							</section>
+							<section class="col-md-3">
+								<div>
+									<i class="fas fa-chalkboard-teacher"></i>
+									<h5>TEACH</h5>
+									<p>I taught UX Design to university students during my Masters Degree.</p>
+								</div>
+							</section>
+							<section class="col-md-3">
+								<div>
+									<i class="fas fa-baseball-ball"></i>
+									<h5>PLAY</h5>
+									<p>	When I am not working, I love to indulge in sports, especially cricket and badminton.</p>
+								</div>
+							</section>
+						</div>
+					</section >
 				</div> <!-- End row -->
 			</div><!-- End container -->
 		</div>
