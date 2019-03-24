10da64e2a068995dd78cc9ad5953316575baa472
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-03-24 17:49:21 +1100

    Update link

--- public/index.php
+++ public/index.php
@@ -307,7 +307,7 @@
 											<a title="Link to Flattr CMS Quiz" href="https://github.com/avinashbarik91/flattr-cms"><i class="fab fa-github"></i></a>
 										</section>
 										<section class="proj-img col-md-12">
-											<a title="Link to Flattr CMS Quiz" href="https://github.com/avinashbarik91/flattr-cms"><img alt="Flattr CMS" src="https://d28qd0hzg413m1.cloudfront.net/images/cms.png"/></a>
+											<a title="Link to Flattr CMS Quiz" href="https://www.flattr-cms.com"><img alt="Flattr CMS" src="https://d28qd0hzg413m1.cloudfront.net/images/cms.png"/></a>
 										</section>
 									</section>
 								</div>
