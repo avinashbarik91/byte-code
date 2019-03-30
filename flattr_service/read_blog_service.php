e63ed9f89abd10165f656b00817ab4127684dbb8
OBXPS\Avinash Barik <avinashbarik91@gmail.com>
2019-03-30 19:19:06 +1100

    Add google analytics code

--- flattr_service/read_blog_service.php
+++ flattr_service/read_blog_service.php
@@ -164,6 +164,17 @@ function render_post_index_html($request_url, $full_path, $blog, $blogs, $prev_b
 	<!DOCTYPE html>
 	<html lang="en">
 	<head>
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136416298-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag("js", new Date());
+
+		  gtag("config", "UA-136416298-1");
+		</script>
+
 		<meta charset="UTF-8">
 		<meta http-equip="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
